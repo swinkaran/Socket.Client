d2974b04a72dd9ae0f785718abac7dfdbeee696f
Swinkaran <swinkaran@hotmail.com>
2016-10-22 12:15:53 +1100

    Asynchronous TCP client message
    Asynchronous TCP client message - Client sending message to the Socker
    server asynchronously

--- Socket-Demo.Client/Program.cs
+++ Socket-Demo.Client/Program.cs
@@ -6,200 +6,84 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using NLog;
+using System.Configuration;
+using System.Security.Permissions;
+using System.IO;
+using NLog.Internal;
 
 namespace Socket_Demo.Client
 {
     class Program
     {
+        private int threadWait = 500;
+
         static void Main(string[] args)
         {
-            Console.Title = "Spcket Client";
-            // Execute teh client application
-            AsynchronousClient.StartClient();
+            Console.Title = "Socket Client";
+
+            // Execute the client application
+            Program p = new Program();
+            p.OnStart();
+            p.OnStop();
+            
+            //AsynchronousClient.StartClient();
         }
-    }
 
-    // State object for receiving data from remote device.
-    public class StateObject
-    {
-        // Client socket.
-        public Socket workSocket = null;
-        // Size of receive buffer.
-        public const int BufferSize = 256;
-        // Receive buffer.
-        public byte[] buffer = new byte[BufferSize];
-        // Received data string.
-        public StringBuilder sb = new StringBuilder();
-    }
-
-    public static class AsynchronousClient
-    {
-        // The port number for the remote device.
-        private const int port = 11000;
-
-        // ManualResetEvent instances signal completion.
-        private static ManualResetEvent connectDone =
-            new ManualResetEvent(false);
-        private static ManualResetEvent sendDone =
-            new ManualResetEvent(false);
-        private static ManualResetEvent receiveDone =
-            new ManualResetEvent(false);
-
-        // The response from the remote device.
-        private static String response = String.Empty;
-
-        public static void StartClient()
+        private void OnStart()
         {
-            // Connect to a remote device.
-            try
-            {
-                // Establish the remote endpoint for the socket.
-                // The name of the 
-                // remote device is "host.contoso.com".
-                IPHostEntry ipHostInfo = Dns.Resolve("10.1.1.9");
-                IPAddress ipAddress = ipHostInfo.AddressList[0];
-                IPEndPoint remoteEP = new IPEndPoint(ipAddress, port);
-
-                // Create a TCP/IP socket.
-                Socket client = new Socket(AddressFamily.InterNetwork,
-                    SocketType.Stream, ProtocolType.Tcp);
-
-                // Connect to the remote endpoint.
-                client.BeginConnect(remoteEP,
-                    new AsyncCallback(ConnectCallback), client);
-                connectDone.WaitOne();
-
-                // Send test data to the remote device.
-                Send(client, "CVB7329I6P,<EOF>");
-                sendDone.WaitOne();
-
-                // Receive the response from the remote device.
-                Receive(client);
-                receiveDone.WaitOne();
-
-                // Write the response to the console.
-                Console.WriteLine("Response received : {0}", response);
-
-                // Release the socket.
-                client.Shutdown(SocketShutdown.Both);
-                client.Close();
-
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+            this.Execute();
         }
-
-        private static void ConnectCallback(IAsyncResult ar)
+    
+        private void OnStop()
         {
-            try
-            {
-                // Retrieve the socket from the state object.
-                Socket client = (Socket)ar.AsyncState;
-
-                // Complete the connection.
-                client.EndConnect(ar);
-
-                Console.WriteLine("Socket connected to {0}",
-                    client.RemoteEndPoint.ToString());
-
-                // Signal that the connection has been made.
-                connectDone.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+            Console.WriteLine("LoyLap Client Stopped");
         }
 
-        private static void Receive(Socket client)
+        [PermissionSet(SecurityAction.Demand, Name = "FullTrust")]
+        private void Execute()
         {
-            try
-            {
-                // Create the state object.
-                StateObject state = new StateObject();
-                state.workSocket = client;
+            // Read the file as one string.
+            string strFileLocation = @"C:\loylap\data";
+            //threadWait = Convert.ToInt16(ConfigurationManager.AppSettings["ThreadWait"]);
 
-                // Begin receiving the data from the remote device.
-                client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                    new AsyncCallback(ReceiveCallback), state);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
+            // Create a new FileSystemWatcher and set its properties.
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = strFileLocation;
+
+            // Watch for changes in LastAccess and LastWrite times, and the renaming of files or directories.
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            // Only watch text files.
+            watcher.Filter = "*.txt";
+
+            // Add event handlers.
+            watcher.Created += new FileSystemEventHandler(OnCreated);
+
+            // Begin watching.
+            watcher.EnableRaisingEvents = true;
+
+            // Wait for the user to quit the program.
+            Console.WriteLine("Press \'q\' to quit the sample.");
+            while (Console.Read() != 'q') ;
         }
 
-        private static void ReceiveCallback(IAsyncResult ar)
+        private void OnCreated(object sender, FileSystemEventArgs e)
         {
-            try
+            if (e.Name.Equals("response.txt"))
             {
-                // Retrieve the state object and the client socket 
-                // from the asynchronous state object.
-                StateObject state = (StateObject)ar.AsyncState;
-                Socket client = state.workSocket;
+                System.Threading.Thread.Sleep(threadWait);
+                
+                string[] requests = System.IO.File.ReadAllLines(e.FullPath);
+                //string request = "\u0002PCVB7329I6P,cbivend00@loylap.com,-0.11\u0003";
+                string request = "\u0002CCVB7329I6P\u0003";
+                //string request = requests[0];
+                Console.WriteLine("New file found here");
+                //delete the file
 
-                // Read data from the remote device.
-                int bytesRead = client.EndReceive(ar);
-
-                if (bytesRead > 0)
-                {
-                    // There might be more data, so store the data received so far.
-                    state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
-
-                    // Get the rest of the data.
-                    client.BeginReceive(state.buffer, 0, StateObject.BufferSize, 0,
-                        new AsyncCallback(ReceiveCallback), state);
-                }
-                else
-                {
-                    // All the data has arrived; put it in response.
-                    if (state.sb.Length > 1)
-                    {
-                        response = state.sb.ToString();
-                    }
-                    // Signal that all bytes have been received.
-                    receiveDone.Set();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
+                File.Delete(@"C:\loylap\data\response.txt");
+                AsynchronousClient.StartClient(request);
             }
         }
-
-        private static void Send(Socket client, String data)
-        {
-            // Convert the string data to byte data using ASCII encoding.
-            byte[] byteData = Encoding.ASCII.GetBytes(data);
-
-            // Begin sending the data to the remote device.
-            client.BeginSend(byteData, 0, byteData.Length, 0,
-                new AsyncCallback(SendCallback), client);
-        }
-
-        private static void SendCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.
-                Socket client = (Socket)ar.AsyncState;
-
-                // Complete sending the data to the remote device.
-                int bytesSent = client.EndSend(ar);
-                Console.WriteLine("Sent {0} bytes to server.", bytesSent);
-
-                // Signal that all bytes have been sent.
-                sendDone.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.ToString());
-            }
-        }
-
     }
 }
 
